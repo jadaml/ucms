f5b55fe1b850d1165c124109f10337afe8e696ef
Ádám L. Juhász <jadaml@gmail.com>
2025-02-25 16:51:41 +0100

    chore: init repo
    - Introducing composer config file
    - Introducing composer file hierarchy
    - Introducing PHP-Markdown 2.0 and PHP-SmartyPants 1.8 dependency
    - Prepare Doxygen
    - Prepare VS Code settings (no tasks or launchers yet)
    - Introduce LICENSE
    - Introduce README.md (half) placeholder
    - Introduce public/index.php placeholder
    - Introduce Doxygen main page placeholder
    - Setup gitignore file with gitignore.io

---
+++ README.md
@@ -0,0 +1,21 @@
+# Micro Content Management System #
+
+*[CMS]: Content Management System
+*[µCMS]: Micro Content Management System
+*[VS Code]: Visual Studio Code
+
+Micro Content Management System (µCMS) is a lightweight CMS software, which allow users to provide the content in Markdown files. The hierarchy of the pages are defined by the file hierarchy the markdown files are placed, and every page is queried with GET requests, thus eliminating the need to maintain a database on the host.
+
+Originally I have created µCMS as a practice to learn PHP in the early 2000s, I have released it as a means to quickly create a CMS website, without much knowledge on building one. Although it is severely inadequate compared to other modern solution, this project remains only as an option to those, who wants to deepen their web server management skill, by introducing a tool that does not provide much help in maintaining the site for them. As technologically it is severely limited, and hence it is called micro-CMS.
+
+When referring to µCMS in a context, where only ASCII characters are available, you may refer to this software as either mCMS or uCMS—though I personally don't like the typesetting of 'µ' as 'u' despite the project package name uses it.
+
+## Setup development environment ##
+
+**TODO** _Describe how to set up VS Code to build this project._
+
+## Install µCMS ##
+
+**TODO** _Describe how to set up µCMS to run on your own server._
+
+**TODO** _Consider additional information in the README.md file._
---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of Micro Content Management System.
+ * 
+ * Micro Content Management System is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * Micro Content Management System is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Micro Content Management System. If not, see <https://www.gnu.org/licenses/>. 
+ */
+/**\file index.php
+ * \brief This file is the main entry point of the application.
+ * \author Ádám Juhász
+ * \copyright GNU General Public License v3
+ * \version 2.0
+ * \date 2025
+ */
+?>
+<footer>
+    <p>Micro Content Management System Copyright &copy; 2025 Ádám Juhász</p>
+    <p>PHP Markdown Lib Copyright &copy; 2004&ndash;2015 Michel Fortin <a href="https://michelf.ca/">https://michelf.ca/</a> All rights reserved.<br/>
+    Based on Markdown Copyright &copy; 2003&ndash;2005 John Gruber <a href="https://daringfireball.net/">https://daringfireball.net/</a> All rights reserved.</p>
+    <p>PHP SmartyPants Lib, Copyright &copy; 2005&ndash;2016 Michel Fortin <a href="https://michelf.ca/">https://michelf.ca/</a> All rights reserved.<br/>
+    SmartyPants, Copyright &copy; 2003&ndash;2004 John Gruber <a href="https://daringfireball.net/">https://daringfireball.net/</a> All rights reserved.</p>
+</footer>
