23895093160fafd987a2867c337dbc5a1997998a
Ádám L. Juhász <jadaml@gmail.com>
2025-02-27 20:46:55 +0100

    fix: naming of ';q' value.

--- public/langs.php
+++ public/langs.php
@@ -3,7 +3,7 @@
 class RequestLanguage {
     private string $language;
     private string $culture;
-    private float $quotient;
+    private float $quality;
 
     public function getLanguage(): string {
         return $this->language;
@@ -14,13 +14,13 @@ class RequestLanguage {
         else return $this->language . '-' . $this->culture;
     }
 
-    public function getQuotient(): float {
-        return $this->quotient;
+    public function getQuality(): float {
+        return $this->quality;
     }
 
     public function __construct(string $language) {
         $language = explode(';', $language);
-        $this->quotient = count($language) == 2 ? floatval(substr($language[1], 2)) : 1.0;
+        $this->quality = count($language) == 2 ? floatval(substr($language[1], 2)) : 1.0;
         $language = explode('-', $language[0]);
         $this->language = $language[0];
         $this->culture = $language[1] ?? '';
@@ -34,7 +34,7 @@ function get_request_language(string $docRoot, ?string $defaultLanguage): string
     }
 
     usort($languages, function(RequestLanguage $a, RequestLanguage $b) {
-        return $b->getQuotient() <=> $a->getQuotient();
+        return $b->getQuality() <=> $a->getQuality();
     });
 
     foreach (scandir($docRoot) as $dir) {
