8845654baf75dd5ba855ff1a9fe220948febec1c
Ádám L. Juhász <jadaml@gmail.com>
2025-03-02 16:34:56 +0100

    style: rename $QSTR to $_PAGE.

--- public/index.php
+++ public/index.php
@@ -26,7 +26,7 @@ include_once __DIR__ . '/../src/md_sp.php';
 include_once __DIR__ . '/../src/navlist.php';
 include_once __DIR__ . '/../src/pages.php';
 
-$QSTR = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+$_PAGE = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
 $_DOCROOT = (isset($DOCROOT) && strlen($DOCROOT) > 0) ? __DIR__ . '/' . $DOCROOT : __DIR__ . '/' . $LANG;
 
 $template = file_get_contents(__DIR__ . '/../resources/template.html');
@@ -35,13 +35,13 @@ if ($template === false) {
     $template = '<!DOCTYPE html><html lang="%LANG%"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>%TITLE%</title><!--%HEAD%---></head><body><p><strong>ERROR</strong> template.html could not be read.</p><header>%HEADER%</header><nav>%NAV%</nav><article>%BODY%</article><footer>%FOOTER%</footer></body></html>';
 }
 
-if (strlen($QSTR) == 0) {
-    $QSTR = $MAINPAGE ?? 'main';
+if (strlen($_PAGE) == 0) {
+    $_PAGE = $MAINPAGE ?? 'main';
 }
 
 $_HEAD = '';
 $_TITLE = $TITLE ?? 'Micro Content Management System?';
-$BODY = get_page($mdParser, $spParser, $_DOCROOT, $QSTR, $LANG, $_HEAD, $_TITLE);
+$BODY = get_page($mdParser, $spParser, $_DOCROOT, $_PAGE, $LANG, $_HEAD, $_TITLE);
 $_COPYNOTE = isset($COPYNOTE) && strlen($COPYNOTE) > 0 ? '<p>' . $COPYNOTE . '</p>' : '';
 $_COPYNOTE .= '<p>Powered by µCMS &copy; 2025 Ádám Juhász</p>';
 $NAV = build_nav_list($mdParser, $spParser, $_DOCROOT, "index.php?", $NAVPAGE ?? null, $LANG);
